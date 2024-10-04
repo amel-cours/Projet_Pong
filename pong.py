b0fea4500851ab10f8b08dc1e8d8cd2997a788a2
amel-cours <bouabdellahamel29@gmail.com>
2024-10-04 11:55:45 +0200

    création de fichiers necessaire au projet

---
+++ pong.py
@@ -0,0 +1,90 @@
+﻿from tkinter import*
+
+def deplacement():
+    global dx, dy
+    #On deplace la balle :
+    can.move(balle1,dx,dy)
+    #On repete cette fonction
+    tk.after(20,deplacement)
+
+#Deplacement de la balle au départ:
+dx=5
+dy=18
+
+#création d'une fenêtre graphique et d'un canevas
+
+fen=Tk()
+tk=Tk()
+fen.title("pong")
+fen.geometry('600x500+100+100')
+Largeur=600
+Hauteur=500
+can= Canvas(fen,width=Largeur, height=Hauteur ,bg='white')
+can.pack(padx=0,pady=0)
+
+#Creation  d'un bouton "Quitter":
+Bouton_Quitter=Button(tk, text ='Quitter', command = tk.destroy)
+#On ajoute l'affichage du bouton dans la fenêtre tk:
+Bouton_Quitter.pack()
+
+#On cree une balle:
+balle1 = can.create_oval(10,10,30,30,fill='pink')
+
+deplacement()
+
+#On lance la boucle principale:
+fen.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ raquette_pong.py
@@ -0,0 +1,32 @@
+﻿from tkinter import*
+
+#Une fonction pour le deplacement vers la droite :
+def droite(event):
+    can.move(raquette,10,0)
+def gauche(event):
+    can.move(raquette,-10,-0.5)
+
+fen=Tk()
+tk=Tk()
+fen.title("pong")
+fen.geometry('600x500+100+100')
+Largeur=600
+Hauteur=500
+can= Canvas(fen,width=Largeur, height=Hauteur ,bg='white')
+can.pack(padx=0,pady=0)
+
+
+#Création  d'un bouton "Quitter":
+Bouton_Quitter=Button(tk, text ='Quitter', command = tk.destroy)
+#On ajoute l'affichage du bouton dans la fenêtre tk:
+Bouton_Quitter.pack()
+
+#On cree une raquette:
+raquette = can.create_rectangle(200,380,300,390,fill='red')
+
+#On associe la touche droite du clavier a la fonction droite():
+can.bind_all('<Right>', droite)
+can.bind_all('<Left>',gauche)
+
+
+fen.mainloop()
